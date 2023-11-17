3d716d1f18bf34141a7e451cb805342e8811b55e
Romain Le Neindre <romain.le.neindre@gmail.com>
2023-11-17 11:17:08 +0100

    Bookmarks

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -6,8 +6,9 @@ class BookmarksController < ApplicationController
   end
 
   def create
-    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
     @bookmark.save
     if @bookmark.save
       redirect_to list_path(@list)
@@ -22,7 +23,6 @@ class BookmarksController < ApplicationController
   private
 
   def bookmark_params
-    params.require(:bookmark).permit(:comment)
+    params.require(:bookmark).permit(:comment, :movie_id)
   end
-
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,7 +5,7 @@ class ListsController < ApplicationController
 
   def show
     @list = List.find(params[:id])
-    @movies = @list.movies
+    @bookmarks = Bookmark.all
   end
 
   def new
